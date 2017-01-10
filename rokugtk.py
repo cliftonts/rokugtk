af3b5bc50e3f0f2f3de01fafd7f01e2a151f3b46
Gareth France <gareth.france@gmail.com>
2017-01-10 21:53:31 +0000

    First release
    This is the first function release of RokuGtk.
    At present it will only scan 192.168.0. range IPs

--- rokugtk.py
+++ rokugtk.py
@@ -2,23 +2,34 @@
 # -*- coding: utf-8 -*-
 
 #Import modules
-import pygtk
-pygtk.require('2.0')
-import gtk
+import sys
+import requests
+import urllib
+if sys.version_info >= (3,0):
+	import urllib.request
+	from gi.repository import Gtk as gtk
+else:
+	import urllib2
+	import pygtk
+	pygtk.require('2.0')
+	import gtk
 
 #Initialise variables
 version = "0.0.1"
+ip = "192.168.0"
 
 class Application():
 	#Initialise window
 	def __init__(self):
 		self.window = gtk.Window()
+		self.window.connect("delete-event", quitting) # no parenthesis; you pass functions to connect
 		self.window.set_title("RokuGtk - " + version)
  
 		self.create_widgets()
 		self.connect_signals()
 
 		self.window.show_all()
+		find()
 		gtk.main()
   
 	def create_widgets(self):
@@ -26,82 +37,81 @@ class Application():
 
 	        #Add buttons
 		#First row
-	        self.hbox_1 = gtk.HBox(spacing=10)
-	        self.button_back = gtk.Button("Back")
-	        self.button_back.set_size_request(1,4)
-	        self.hbox_1.pack_start(self.button_back)
+		self.hbox_1 = gtk.HBox(spacing=10)
+		self.button_back = gtk.Button("Back")
+		self.button_back.set_size_request(1,4)
+		self.hbox_1.pack_start(self.button_back, True, True, 0)
 
-	        self.button_up = gtk.Button("Up")
-	        self.button_up.set_size_request(1,4)
-	        self.hbox_1.pack_start(self.button_up)
+		self.button_up = gtk.Button("Up")
+		self.button_up.set_size_request(1,4)
+		self.hbox_1.pack_start(self.button_up, True, True, 0)
      
-	        self.button_home = gtk.Button("Home")
-	        self.button_home.set_size_request(1,4)
-	        self.hbox_1.pack_start(self.button_home)
+		self.button_home = gtk.Button("Home")
+		self.button_home.set_size_request(1,4)
+		self.hbox_1.pack_start(self.button_home, True, True, 0)
 
 		#Second row
 		self.hbox_2 = gtk.HBox(spacing=10)
-	        self.button_left = gtk.Button("Left")
-	        self.button_left.set_size_request(1,4)
-	        self.hbox_2.pack_start(self.button_left)
+		self.button_left = gtk.Button("Left")
+		self.button_left.set_size_request(1,4)
+		self.hbox_2.pack_start(self.button_left, True, True, 0)
 
 		self.button_select = gtk.Button("Select")
-	        self.button_select.set_size_request(1,4)
-	        self.hbox_2.pack_start(self.button_select)
+		self.button_select.set_size_request(1,4)
+		self.hbox_2.pack_start(self.button_select, True, True, 0)
 
 		self.button_right = gtk.Button("Right")
-	        self.button_right.set_size_request(1,4)
-	        self.hbox_2.pack_start(self.button_right)
+		self.button_right.set_size_request(1,4)
+		self.hbox_2.pack_start(self.button_right, True, True, 0)
 
 		#Third row
 		self.hbox_3 = gtk.HBox(spacing=10)
-	        self.button_reverse = gtk.Button("Reverse")
-	        self.button_reverse.set_size_request(1,4)
-	        self.hbox_3.pack_start(self.button_reverse)
+		self.button_reverse = gtk.Button("Reverse")
+		self.button_reverse.set_size_request(1,4)
+		self.hbox_3.pack_start(self.button_reverse, True, True, 0)
 
 		self.button_down = gtk.Button("Down")
-	        self.button_down.set_size_request(1,4)
-	        self.hbox_3.pack_start(self.button_down)
+		self.button_down.set_size_request(1,4)
+		self.hbox_3.pack_start(self.button_down, True, True, 0)
 
 		self.button_forward = gtk.Button("Forward")
-	        self.button_forward.set_size_request(1,4)
-	        self.hbox_3.pack_start(self.button_forward)
+		self.button_forward.set_size_request(1,4)
+		self.hbox_3.pack_start(self.button_forward, True, True, 0)
 
 		#Fourth row
 		self.hbox_4 = gtk.HBox(spacing=10)
-	        self.button_play = gtk.Button("Play")
-	        self.button_play.set_size_request(1,4)
-	        self.hbox_4.pack_start(self.button_play)
+		self.button_play = gtk.Button("Play")
+		self.button_play.set_size_request(1,4)
+		self.hbox_4.pack_start(self.button_play, True, True, 0)
 
 		self.button_reload = gtk.Button("Reload")
-	        self.button_reload.set_size_request(1,4)
-	        self.hbox_4.pack_start(self.button_reload)
+		self.button_reload.set_size_request(1,4)
+		self.hbox_4.pack_start(self.button_reload, True, True, 0)
 
 		#Fifth row
 		self.hbox_5 = gtk.HBox(spacing=10)
-	        self.button_info = gtk.Button("Info")
-	        self.button_info.set_size_request(1,4)
-	        self.hbox_5.pack_start(self.button_info)
+		self.button_info = gtk.Button("Info")
+		self.button_info.set_size_request(1,4)
+		self.hbox_5.pack_start(self.button_info, True, True, 0)
 
 		self.button_search = gtk.Button("Search")
-	        self.button_search.set_size_request(1,4)
-	        self.hbox_5.pack_start(self.button_search)
-
+		self.button_search.set_size_request(1,4)
+		self.hbox_5.pack_start(self.button_search, True, True, 0)
  
-	        self.vbox.pack_start(self.hbox_1)
-		self.vbox.pack_start(self.hbox_2)
-		self.vbox.pack_start(self.hbox_3)
-		self.vbox.pack_start(self.hbox_4)
-		self.vbox.pack_start(self.hbox_5)
+		self.vbox.pack_start(self.hbox_1, True, True, 0)
+		self.vbox.pack_start(self.hbox_2, True, True, 0)
+		self.vbox.pack_start(self.hbox_3, True, True, 0)
+		self.vbox.pack_start(self.hbox_4, True, True, 0)
+		self.vbox.pack_start(self.hbox_5, True, True, 0)
 	 
-	        self.window.add(self.vbox)
-	        self.window.set_size_request(400, 400)
+		self.window.add(self.vbox)
+		self.window.set_size_request(400, 400)
 
 
 	def connect_signals(self):
-	        self.button_back.connect("clicked", self.callback_back)
-	        self.button_up.connect("clicked", self.callback_up)
-	        self.button_home.connect("clicked", self.callback_home)
+		self.button_back.connect("clicked", self.callback_back)
+		self.button_up.connect("clicked", self.callback_up)
+		self.button_home.connect("clicked", self.callback_home)
 
 		self.button_left.connect("clicked", self.callback_left)
 		self.button_select.connect("clicked", self.callback_select)
@@ -118,139 +128,92 @@ class Application():
 	 
  
 	def callback_back(self, widget, callback_data=None):
-	        #name = self.entry.get_text()
-	        #print name
-	        dialog = gtk.FileChooserDialog("Open..",
-                               None,
-                               gtk.FILE_CHOOSER_ACTION_OPEN,
-                               (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-                                gtk.STOCK_OPEN, gtk.RESPONSE_OK))
-	        dialog.set_default_response(gtk.RESPONSE_OK)
-
-	        filter = gtk.FileFilter()
-	        filter.set_name("All files")
-	        filter.add_pattern("*")
-	        dialog.add_filter(filter)
-
-	        filter = gtk.FileFilter()
-	        filter.set_name("Images")
-	        filter.add_mime_type("image/png")
-	        filter.add_mime_type("image/jpeg")
-	        filter.add_mime_type("image/gif")
-	        filter.add_pattern("*.png")
-	        filter.add_pattern("*.jpg")
-	        filter.add_pattern("*.gif")
-	        filter.add_pattern("*.tif")
-	        filter.add_pattern("*.xpm")
-	        dialog.add_filter(filter)
-	        dialog.set_current_folder("~/")
-	
-	        response = dialog.run()
-	        if response == gtk.RESPONSE_OK:
-	            global inputfile
-	            inputfile = dialog.get_filename()
-	            #inputfile = inputfile.replace(" ", "\ ")
-	            #print inputfile
-	        #elif response == gtk.RESPONSE_CANCEL:
-	            #print 'Closed, no files selected'
-	        dialog.destroy()
+		cmd = "http://" + ip + ":8060/keypress/Back"
+		send(cmd)
  
  
 	def callback_up(self, widget, callback_data=None):
-	        #gtk.main_quit()
-	        dialog = gtk.FileChooserDialog("Save..",
-	                               None,
-	                               gtk.FILE_CHOOSER_ACTION_SAVE,
-	                               (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-	                                gtk.STOCK_OPEN, gtk.RESPONSE_OK))
-	        dialog.set_default_response(gtk.RESPONSE_OK)
-	
-	        filter = gtk.FileFilter()
-	        filter.set_name("All files")
-	        filter.add_pattern("*")
-	        dialog.add_filter(filter)
-
-	        filter = gtk.FileFilter()
-	        filter.set_name("Images")
-	        filter.add_mime_type("image/png")
-	        filter.add_mime_type("image/jpeg")
-	        filter.add_mime_type("image/gif")
-	        filter.add_pattern("*.png")
-	        filter.add_pattern("*.jpg")
-	        filter.add_pattern("*.gif")
-	        filter.add_pattern("*.tif")
-	        filter.add_pattern("*.xpm")
-	        dialog.add_filter(filter)
-	        dialog.set_current_folder("~/")
-	
-	        response = dialog.run()
-	        if response == gtk.RESPONSE_OK:
-	            global outputfile
-	            outputfile = dialog.get_filename()
-	            #outputfile = outputfile.replace(" ", "\ ")
-	        #elif response == gtk.RESPONSE_CANCEL:
-	            #print 'Closed, no files selected'
-	        dialog.destroy() 
+		cmd = "http://" + ip + ":8060/keypress/Up"
+		send(cmd)
 
 	def callback_home(self, widget, callback_data=None):
-	        global inputfile
-	        global outputfile
-	        global test
-	        if port == "File" and inputfile == "":
-	           message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR)
-	           message.set_markup("Check input file selection and try again.")
-	           message.run()
-	        else:
-	           if outputfile != "" and port != "" and test != "":
-	              testmode = "--t=" + test
-	              method = "--m=std"
-	              inputfile2 = "--i=" + inputfile
-	              inputfile=inputfile2
-	              outputfile = "--o=" + outputfile
-	              if cts == "True":
-	                 method = "--m=cts"
-	              if port == "File":                 
-	                 cmd = "/opt/cliftontestsuite/cliftontestsuite "+ testmode + " \"" + inputfile + "\" \"" + outputfile + "\"" + " " + method
-	              else:
-	                 cmd = "gksu /opt/cliftontestsuite/cliftontestsuite "+ testmode + " " + port + " \"" + outputfile + "\" " + method
-	              #print cmd
-	              #os.system(cmd)
-	              call (cmd, shell=True)
-	           else:
-	              message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR)
-	              message.set_markup("Check\nTester Model\nChosen Port\nInput and output file selections\nand try again.")
-	              message.run()
+		cmd = "http://" + ip + ":8060/keypress/Home"
+		send(cmd)
 
 	def callback_left(self, widget, callback_data=None):
-		print ("Left")
+		cmd = "http://" + ip + ":8060/keypress/Left"
+		send(cmd)
 
 	def callback_select(self, widget, callback_data=None):
-		print ("Select")
+		cmd = "http://" + ip + ":8060/keypress/Select"
+		send(cmd)
 
 	def callback_right(self, widget, callback_data=None):
-		print ("Right")
+		cmd = "http://" + ip + ":8060/keypress/Right"
+		send(cmd)
 
 	def callback_reverse(self, widget, callback_data=None):
-		print ("Reverse")
+		cmd = "http://" + ip + ":8060/keypress/Rev"
+		send(cmd)
 
 	def callback_down(self, widget, callback_data=None):
-		print ("Down")
+		cmd = "http://" + ip + ":8060/keypress/Down"
+		send(cmd)
 
 	def callback_forward(self, widget, callback_data=None):
-		print ("Forward")
+		cmd = "http://" + ip + ":8060/keypress/Fwd"
+		send(cmd)
 
 	def callback_play(self, widget, callback_data=None):
-		print ("Play")
+		cmd = "http://" + ip + ":8060/keypress/Play"
+		send(cmd)
 
 	def callback_reload(self, widget, callback_data=None):
-		print ("Reload")
+		cmd = "http://" + ip + ":8060/keypress/InstantReplay"
+		send(cmd)
 
 	def callback_info(self, widget, callback_data=None):
-		print ("Info")
+		cmd = "http://" + ip + ":8060/keypress/Info"
+		send(cmd)
 
 	def callback_search(self, widget, callback_data=None):
 		print ("Search")
+
+def send(url):
+	payload = {'': ''}
+	try:
+		# POST with form-encoded data
+		r = requests.post(url, data=payload)
+	
+		# Response, status etc
+		#r.text
+		#r.status_code
+	except:
+		print ("ROKU NOT FOUND!")
         
+
+def find():
+	global ip
+	for i in range(1,256):
+		try:
+			url = 'http://' + ip + '.' + str(i) + ':8060'
+			print ("Attempting - " + ip + "." + str(i))	
+			r = urllib2.urlopen(url, timeout=0.1)
+			html=r.read()
+			if "Roku" in html:
+				print ("Roku found!")
+				print (ip + "." + str(i))
+				ip = ip + "." + str(i)
+				break
+		except:
+			pass
+
+#This routine will allow for a pop up window offering donate info upon quit.
+def quitting(tmp, tmp2):
+	print (tmp)
+	print (tmp2)
+	gtk.main_quit()
+
 
 if __name__ == "__main__":
     app = Application()
