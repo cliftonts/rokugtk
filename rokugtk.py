5d941b6343b1c2e3b7aefc4f34f40622f201b984
Peter <git@linmob.net>
2021-07-22 17:38:50 +0200

    attempted modernization with 2to3 and fixing errors afterwards

--- rokugtk.py
+++ rokugtk.py
@@ -6,25 +6,27 @@ import socket
 import os
 import sys
 import requests
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import time
 import ssdp
+import gi
 if sys.version_info >= (3,0):
 	import urllib.request
-	from gi.repository import Gtk as gtk
+	gi.require_version('Gtk', '3.0')
+	from gi.repository import Gtk as Gtk
 else:
-	import urllib2
-	import pygtk
-	pygtk.require('2.0')
-	import gtk
+	import urllib.request, urllib.error, urllib.parse
+	import pyGtk
+	gi.require_version('Gtk', '3.0')
+	import Gtk
 
 
 #Initialise variables
-version = "0.4.0" #Also change in snapcraft.yaml and setup/gui/rokugtk.desktop
+version = "0.4.0" #Also change in snapcraft.yaml and setup/gui/rokuGtk.desktop
 ip = ""
 
 def get_resource_path(rel_path):
-	"""https://stackoverflow.com/questions/4416336/adding-a-program-icon-in-python-gtk"""
+	"""https://stackoverflow.com/questions/4416336/adding-a-program-icon-in-python-Gtk"""
 	dir_of_py_file = os.path.dirname(__file__)
 	rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
 	abs_path_to_resource = os.path.abspath(rel_path_to_resource)
@@ -33,8 +35,8 @@ def get_resource_path(rel_path):
 class Application():
 	#Initialise window
 	def __init__(self):
-		self.window = gtk.Window()
-		self.window.connect("delete-event", quitting) # no parenthesis; you pass functions to connect
+		self.window = Gtk.Window()
+		self.window.connect("destroy", Gtk.main_quit) # no parenthesis; you pass functions to connect
 		self.window.set_title("RokuGtk - " + version)
 		self.window.set_icon_from_file(get_resource_path("rokugtk.png"))
 
@@ -43,76 +45,76 @@ class Application():
 
 		self.window.show_all()
 		response = ssdp.discover("roku:ecp")
-		ip = response[0].location
-		gtk.main()
+		ip = response[0].location.encode()
+		Gtk.main()
 
 	def create_widgets(self):
-		self.vbox = gtk.VBox(spacing=10)
+		self.vbox = Gtk.VBox(spacing=10)
 
 	        #Add buttons
 		#First row
-		self.hbox_1 = gtk.HBox(spacing=10)
-		self.button_back = gtk.Button("Back")
+		self.hbox_1 = Gtk.HBox(spacing=10)
+		self.button_back = Gtk.Button.new_with_label("Back")
 		self.button_back.set_size_request(1,4)
 		self.hbox_1.pack_start(self.button_back, True, True, 0)
 
-		self.button_up = gtk.Button("Up")
+		self.button_up = Gtk.Button.new_with_label("Up")
 		self.button_up.set_size_request(1,4)
 		self.hbox_1.pack_start(self.button_up, True, True, 0)
 
-		self.button_home = gtk.Button("Home")
+		self.button_home = Gtk.Button.new_with_label("Home")
 		self.button_home.set_size_request(1,4)
 		self.hbox_1.pack_start(self.button_home, True, True, 0)
 
 		#Second row
-		self.hbox_2 = gtk.HBox(spacing=10)
-		self.button_left = gtk.Button("Left")
+		self.hbox_2 = Gtk.HBox(spacing=10)
+		self.button_left = Gtk.Button.new_with_label("Left")
 		self.button_left.set_size_request(1,4)
 		self.hbox_2.pack_start(self.button_left, True, True, 0)
 
-		self.button_select = gtk.Button("Select")
+		self.button_select = Gtk.Button.new_with_label("Select")
 		self.button_select.set_size_request(1,4)
 		self.hbox_2.pack_start(self.button_select, True, True, 0)
 
-		self.button_right = gtk.Button("Right")
+		self.button_right = Gtk.Button.new_with_label("Right")
 		self.button_right.set_size_request(1,4)
 		self.hbox_2.pack_start(self.button_right, True, True, 0)
 
 		#Third row
-		self.hbox_3 = gtk.HBox(spacing=10)
-		self.button_reverse = gtk.Button("Reverse")
+		self.hbox_3 = Gtk.HBox(spacing=10)
+		self.button_reverse = Gtk.Button.new_with_label("Reverse")
 		self.button_reverse.set_size_request(1,4)
 		self.hbox_3.pack_start(self.button_reverse, True, True, 0)
 
-		self.button_down = gtk.Button("Down")
+		self.button_down = Gtk.Button.new_with_label("Down")
 		self.button_down.set_size_request(1,4)
 		self.hbox_3.pack_start(self.button_down, True, True, 0)
 
-		self.button_forward = gtk.Button("Forward")
+		self.button_forward = Gtk.Button.new_with_label("Forward")
 		self.button_forward.set_size_request(1,4)
 		self.hbox_3.pack_start(self.button_forward, True, True, 0)
 
 		#Fourth row
-		self.hbox_4 = gtk.HBox(spacing=10)
-		self.button_play = gtk.Button("Play")
+		self.hbox_4 = Gtk.HBox(spacing=10)
+		self.button_play = Gtk.Button.new_with_label("Play")
 		self.button_play.set_size_request(1,4)
 		self.hbox_4.pack_start(self.button_play, True, True, 0)
 
-		self.button_reload = gtk.Button("Reload")
+		self.button_reload = Gtk.Button.new_with_label("Reload")
 		self.button_reload.set_size_request(1,4)
 		self.hbox_4.pack_start(self.button_reload, True, True, 0)
 
 		#Fifth row
-		self.hbox_5 = gtk.HBox(spacing=10)
-		self.button_info = gtk.Button("Info")
+		self.hbox_5 = Gtk.HBox(spacing=10)
+		self.button_info = Gtk.Button.new_with_label("Info")
 		self.button_info.set_size_request(1,4)
 		self.hbox_5.pack_start(self.button_info, True, True, 0)
 
-		self.button_search = gtk.Button("Search")
+		self.button_search = Gtk.Button.new_with_label("Search")
 		self.button_search.set_size_request(1,4)
 		self.hbox_5.pack_start(self.button_search, True, True, 0)
 
-		self.button_about = gtk.Button("About")
+		self.button_about = Gtk.Button.new_with_label("About")
 		self.button_about.set_size_request(1,4)
 		self.hbox_5.pack_start(self.button_about, True, True, 0)
 
@@ -198,8 +200,8 @@ class Application():
 	def callback_search(self, widget, callback_data=None):
 		search = EntryDialog(parent=None, 
                             flags=0, 
-                            type=gtk.MESSAGE_INFO, 
-                            buttons=gtk.BUTTONS_OK, 
+                            type=Gtk.MESSAGE_INFO, 
+                            buttons=Gtk.BUTTONS_OK, 
                             message_format=None)
 		search.set_markup("Search")
 		searchfor = search.run()
@@ -208,31 +210,31 @@ class Application():
 			keyboard(ip, searchfor)
 
 	def callback_about(self, widget, callback_data=None):
-		aboutdialog = gtk.AboutDialog()
+		aboutdialog = Gtk.AboutDialog()
 		# lists of authors and documenters (will be used later)
-        	authors = ["Gareth France"]
+		authors = ["Gareth France"]
 
         	# we fill in the aboutdialog
-        	aboutdialog.set_program_name("RokuGtk " + version)
+		aboutdialog.set_program_name("RokuGtk " + version)
 		aboutdialog.set_comments("RokuGtk is a remote control application for controlling Roku and Now TV set top boxes.\n\nIf you have found this program to be useful please consider making a small donation towards future development.\nPaypal:- gareth.france@gmail.com\nPPPay.com:- gareth.france@cliftonts.co.uk\n\n A massive thank you to kyrofa, elopio and Mark Shuttleworth for their help in making the snap version possible.")
-        	aboutdialog.set_authors(authors)
-        	aboutdialog.set_website("https://github.com/cliftonts/rokugtk")
-        	aboutdialog.set_website_label("Report issues on GitHub")
+		aboutdialog.set_authors(authors)
+		aboutdialog.set_website("https://github.com/cliftonts/rokuGtk")
+		aboutdialog.set_website_label("Report issues on GitHub")
 
-        	# we do not want to show the title, which by default would be "About AboutDialog Example"
-        	# we have to reset the title of the messagedialog window after setting
-        	# the program name
-        	aboutdialog.set_title("")
+		# we do not want to show the title, which by default would be "About AboutDialog Example"
+		# we have to reset the title of the messagedialog window after setting
+		# the program name
+		aboutdialog.set_title("")
 
         	# to close the aboutdialog when "close" is clicked we connect the
         	# "response" signal to on_close
-        	aboutdialog.connect("response", self.on_close)
+		aboutdialog.connect("response", self.on_close)
         	# show the aboutdialog
-        	aboutdialog.show()
+		aboutdialog.show()
 
     		# destroy the aboutdialog
-    	def on_close(self, action, parameter):
-    	    action.destroy()
+		def on_close(self, action, parameter):
+			action.destroy()
 		
 
 def send(url):
@@ -245,10 +247,10 @@ def send(url):
 		#r.text
 		#r.status_code
 	except:
-		message = gtk.MessageDialog(parent=None, 
+		message = Gtk.MessageDialog(parent=None, 
                             flags=0, 
-                            type=gtk.MESSAGE_WARNING, 
-                            buttons=gtk.BUTTONS_OK, 
+                            type=Gtk.MESSAGE_WARNING, 
+                            buttons=Gtk.BUTTONS_OK, 
                             message_format=None)
 		message.set_markup("Roku not found!\nPlease try again.")
 		message.run()
@@ -271,27 +273,27 @@ def find():
 	for i in range(1,257):
 		try:
 			url = 'http://' + ip + str(i) + ':8060'
-			print ("Attempting - " + ip + str(i))
-			r = urllib2.urlopen(url, timeout=0.2)
+			print(("Attempting - " + ip + str(i)))
+			r = urllib.request.urlopen(url, timeout=0.2)
 			html=r.read()
 			if "Roku" in html:
 				print ("Roku found!")
-				print (ip + str(i))
+				print((ip + str(i)))
 				ip = ip + str(i)
 				break
 		except:
 			pass
 	if ip[-1:] == ".":
-		message = gtk.MessageDialog(parent=None, 
+		message = Gtk.MessageDialog(parent=None, 
                             flags=0, 
-                            type=gtk.MESSAGE_WARNING, 
-                            buttons=gtk.BUTTONS_OK, 
+                            type=Gtk.MESSAGE_WARNING, 
+                            buttons=Gtk.BUTTONS_OK, 
                             message_format=None)
 		message.set_markup("Roku not found!\nPlease try again.")
 		message.run()
 		quit()
 
-class EntryDialog(gtk.MessageDialog):
+class EntryDialog(Gtk.MessageDialog):
     def __init__(self, *args, **kwargs):
         '''
         Creates a new EntryDialog. Takes all the arguments of the usual
@@ -304,11 +306,11 @@ class EntryDialog(gtk.MessageDialog):
         else:
             default_value = ''
         super(EntryDialog, self).__init__(*args, **kwargs)
-        entry = gtk.Entry()        
+        entry = Gtk.Entry()        
         entry.set_text(str(default_value))
         entry.connect("activate", 
                       lambda ent, dlg, resp: dlg.response(resp), 
-                      self, gtk.RESPONSE_OK)
+                      self, Gtk.RESPONSE_OK)
         self.vbox.pack_end(entry, True, True, 0)
         self.vbox.show_all()
         self.entry = entry
@@ -316,7 +318,7 @@ class EntryDialog(gtk.MessageDialog):
         self.entry.set_text(text)
     def run(self):
         result = super(EntryDialog, self).run()
-        if result == gtk.RESPONSE_OK:
+        if result == Gtk.RESPONSE_OK:
             text = self.entry.get_text()
         else:
             text = None
@@ -331,7 +333,7 @@ def keyboard(ip, keyin):
 		if sys.version_info >= (3,0):
 			url = "http://" + ip + ":8060/keypress/Lit_" + urllib.parse.quote(i, safe='');
 		else:
-			url = "http://" + ip + ":8060/keypress/Lit_" + urllib.quote(i, safe='');
+			url = "http://" + ip + ":8060/keypress/Lit_" + urllib.parse.quote(i, safe='');
 		payload = {'': ''}
 		try:
 			# POST with form-encoded data
@@ -341,10 +343,10 @@ def keyboard(ip, keyin):
 			#r.text
 			#r.status_code
 		except:
-			message = gtk.MessageDialog(parent=None, 
+			message = Gtk.MessageDialog(parent=None, 
                             flags=0, 
-                            type=gtk.MESSAGE_WARNING, 
-                            buttons=gtk.BUTTONS_OK, 
+                            type=Gtk.MESSAGE_WARNING, 
+                            buttons=Gtk.BUTTONS_OK, 
                             message_format=None)
 			message.set_markup("Roku not found!\nPlease try again.")
 			message.run()
@@ -355,7 +357,7 @@ def keyboard(ip, keyin):
 
 #This routine will allow for any code to run before quitting.
 def quitting(tmp, tmp2):
-	gtk.main_quit()
+	Gtk.main_quit()
 
 
 if __name__ == "__main__":
